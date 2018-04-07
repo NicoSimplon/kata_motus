17315ea2be7053f7de9446af84681f82f9cf7a85
NicoSimplon <marty.nicolas11@laposte.net>
2018-04-07 20:16:15 +0200

    jeu fonctionnel

--- main.js
+++ main.js
@@ -36,23 +36,33 @@ var numRandom = Math.floor(Math.random()*arr.length);
 
 var motRandom = arr[numRandom];
 
+var alea = motRandom.split("");
+
 console.log(motRandom);
 
 // Ma variable permettant de limiter le nombre de chances du joueur
 var chance = 7;
 
+$("#rejouer").hide();
+
+//J'affiche la première lettre du mot à trouver
+$("#indice").html('<span>'+alea[0]+'.......</span>');
+
 function rejouer(){
-
-	$("#boutons").html('<input id="rejouer" type="button" value="Rejouer">');
-
+	$("#valider").hide();
+	$("#rejouer").show();
 	$("#rejouer").click(function(){
 		chance = 7;
 		numRandom = Math.floor(Math.random()*arr.length);
 		motRandom = arr[numRandom];
-		$("#boutons").html('<input type="button" id="valider" name="valider" value="Valider">');
+		alea = motRandom.split("");
+		$("#rejouer").hide();
+		$("#valider").show();
 		$("#affichage").text('');
+		$("#indice").html('<span>'+alea[0]+'.......</span>');
+
 		console.log(motRandom);
-		console.log(chance);
+		//console.log(chance);
 	})
 	
 }
@@ -61,59 +71,74 @@ function rejouer(){
 $("#valider").click(function(){
 
 	chance--;
-	console.log(chance);
+	//console.log(chance);
+	
 	// Je récupère le mot rentré par le joueur dans l'input
 	// Et je transforme mes deux mots en array
 
 	recup = $("#reponse").val();
 	
-	var un = recup.split("");
+	var rep = recup.split("");
 	
-	var deux = motRandom.split("");
+	var alea = motRandom.split("");
 	
-	// console.log(un);
-	// console.log(deux);
+	// console.log(rep);
+	// console.log(alea);
 
 	// Je compare le mot du joueur et le mot sélectionné aléatoirement
 	// dans une boucle afin de les comparer lettre par lettre
 	
 	var i = 0;
-	if(un.length === 7){
+	
 	while(i<8){
 		
 		//Afin d'afficher le mot rentré par le joueur je rajoute un br 
 		// lorsque la comparaison porte sur la dernière lettre du mot
 		
-			if(i === 7){
-				if(un[i] == deux[i]) {
-					$("#affichage").append('<span class="green">'+un[i]+'</span><br>');
+			if(i === 7 && rep.length == 8){
+				if(rep[i] == alea[i]) {
+					$("#affichage").append('<span class="green">'+rep[i]+'</span><br>');
 				}
-				else if(un[i] != deux[i]){
-					$("#affichage").append('<span class="red">'+un[i]+'</span><br>');
+				else if(rep[i] != alea[i]){
+					var verif = alea.includes(rep[i]);
+					if(verif == true){
+						$("#affichage").append('<span class="orange">'+rep[i]+'</span><br>');
+					}
+					else{
+						$("#affichage").append('<span class="red">'+rep[i]+'</span><br>');
+					}
+					
 				}
 			}
-			else if(i < 7) {
-				if(un[i] == deux[i]) {
-					$("#affichage").append('<span class="green">'+un[i]+'</span>');
+			else if(i < 7 && rep.length == 8) {
+				if(rep[i] == alea[i]) {
+					$("#affichage").append('<span class="green">'+rep[i]+'</span>');
 				}
-				else if(un[i] != deux[i]){
-					$("#affichage").append('<span class="red">'+un[i]+'</span>');
+				else if(rep[i] != alea[i]){
+					var verif = alea.includes(rep[i]);
+					if(verif == true){
+						$("#affichage").append('<span class="orange">'+rep[i]+'</span>');
+					}
+					else{
+						$("#affichage").append('<span class="red">'+rep[i]+'</span>');
+					}
 				}
 			}
 
 			// J'incrémente mon itérateur
 			i++;
-		}
-			
 
 	}
 	
-	// Je vide le champs où le joueur rentre sa réponse
-	
+	// Je vide le champs où le joueur rentre sa réponse	
 	$("#reponse").val("");
 
 	if(chance === 0 && recup != motRandom){
-		$("#valider").hide();
+		alert("Vous avez perdu! Le mot était: "+motRandom);
+		rejouer();
+	}
+	else if (chance >= 0 && recup == motRandom){
+		alert("Félicitations! Vous avez trouvé le bon mot: "+recup);
 		rejouer();
 	}
 
